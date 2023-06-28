778b70966fc7dafde2ba71533dc9fc4f8abd7876
youngsoAN <127369546+youngsoAN@users.noreply.github.com>
2023-06-28 15:36:00 +0900

    Update my_portfolio_about.js

--- my_portfolio_about.js
+++ my_portfolio_about.js
@@ -3,8 +3,10 @@ $(document).ready(function () {
     var scroll = $(window).scrollTop();
     if (scroll > 1) {
       $("header").css("background", "#ffff");
+      $("header").css("color", "#c75f5f");
     } else {
       $("header").css("background", "#ffdbdb");
+      $("header").css("color", "#000");
     }
   });
 });
