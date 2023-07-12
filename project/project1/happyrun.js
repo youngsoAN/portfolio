b27b8f42b30f0c8fe2bb9457115a7ba128f0fbea
youngsoAN <whirosa@naver.com>
2023-07-12 17:28:44 +0900

    js 주석처리

--- project/project1/happyrun.js
+++ project/project1/happyrun.js
@@ -18,40 +18,40 @@ $(".contact").on("scroll", () => {});
 //   $('.slide_box1').style.
 // });
 
-let current = 0;
-let total = $(".slide_wrapper .slide_item").length;
+// let current = 0;
+// let total = $(".slide_wrapper .slide_item").length;
 
-const slide = (item, idx) => {
-  // item: next, prev / idx: current
-  let i = current;
+// const slide = (item, idx) => {
+//   // item: next, prev / idx: current
+//   let i = current;
 
-  if (idx > total) {
-    // 첫화면
-    i = 0;
-    current = 0;
-  } else if (idx < 0) {
-    // 마지막 화면
-    i = total - 1;
-    current = total - 1;
-  }
+//   if (idx > total) {
+//     // 첫화면
+//     i = 0;
+//     current = 0;
+//   } else if (idx < 0) {
+//     // 마지막 화면
+//     i = total - 1;
+//     current = total - 1;
+//   }
 
-  $(".slide_wrapper .slide_item").removeClass("active");
-  $(".slide_wrapper .slide_item").eq(i).addClass("active");
-};
+//   $(".slide_wrapper .slide_item").removeClass("active");
+//   $(".slide_wrapper .slide_item").eq(i).addClass("active");
+// };
 
-const autoSlide = () => {
-  let next = current;
-  current = current + 1;
-  slide(next, current);
-};
+// const autoSlide = () => {
+//   let next = current;
+//   current = current + 1;
+//   slide(next, current);
+// };
 
-// 넥스트 버튼 클릭시 슬라이드
-$("#nextBtn").on("click", autoSlide);
+// // 넥스트 버튼 클릭시 슬라이드
+// $("#nextBtn").on("click", autoSlide);
 
-$("#prevBtn").on("click", () => {
-  let prev = current; // 현재 위치 인덱스
-  current = current - 1; // 이전 위치 인덱스
-  slide(prev, current);
-});
+// $("#prevBtn").on("click", () => {
+//   let prev = current; // 현재 위치 인덱스
+//   current = current - 1; // 이전 위치 인덱스
+//   slide(prev, current);
+// });
 
-setInterval(autoSlide, 3000);
+// setInterval(autoSlide, 3000);
