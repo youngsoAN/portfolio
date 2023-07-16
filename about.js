436e325db030a02b26125f13a4c72847cba9ed18
youngsoAN <127369546+youngsoAN@users.noreply.github.com>
2023-07-17 01:08:49 +0900

    about 나타나기 효과
    나타나기 효과 새로 적용시켜봄

--- about.js
+++ about.js
@@ -11,16 +11,36 @@ $(document).ready(function () {
   });
 });
 
-const moving = (e) => {
-  $(".dance").css({
-    left: e.clientX - 40 + "px",
-    top: e.clientY - 100 + "px",
-  });
-};
+//
+// const moving = (e) => {
+//   $(".dance").css({
+//     left: e.clientX - 40 + "px",
+//     top: e.clientY - 100 + "px",
+//   });
+// };
 
-$(document).on("mousemove", () => {
-  $(document).on("mousemove", moving);
-  $(document).on("mouseup", () => {});
+// $(document).on("mousemove", () => {
+//   $(document).on("mousemove", moving);
+//   $(document).on("mouseup", () => {});
+// });
+//
+
+const circle = document.querySelector(".dance");
+
+document.addEventListener("mousemove", (e) => {
+  // mousemove이벤트를 이용해 움
+
+  // 마우스의 좌표는 clientX와 clientY를 이용해 알수 있다. -> 브라우저 window의 좌표값 위치를 전달한다.
+
+  // pageX, pageY와는 다름.
+
+  const mouseX = e.clientX;
+
+  const mouseY = e.clientY;
+
+  circle.style.left = mouseX + "px";
+
+  circle.style.top = mouseY + "px";
 });
 
 $(window).scroll(function () {
@@ -31,4 +51,15 @@ $(window).scroll(function () {
   //   $(".skillBox").fadeOut(500);
   //   console.log("페이드아웃");
   // }
+});
+
+$(window).scroll(function () {
+  $(".skillBox").each(function (i) {
+    var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+    var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+    if (bottom_of_window > bottom_of_element) {
+      $(this).animate({ opacity: "1" }, 900);
+    }
+  });
 });
