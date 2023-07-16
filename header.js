89645f8adf521aa58ef48a75c0c9c07db17b65ca
youngsoAN <127369546+youngsoAN@users.noreply.github.com>
2023-07-17 01:08:20 +0900

    header html,css 생성
    반복적으로 사용되는 헤더와 푸터를 따로 저장해둠

---
+++ header.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll > 1) {
+      $("header").css("background", "#ffff");
+      $("header").css("color", "#c75f5f");
+    } else {
+      $("header").css("background", "#ffdbdb");
+      $("header").css("color", "#000");
+    }
+  });
+});
