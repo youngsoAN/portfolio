29e737306f857708bb453dcf4856109748e711ba
youngsoAN <whirosa@naver.com>
2023-08-02 13:09:31 +0900

    햄버거 메뉴 추가
    js 이벤트 추가

--- project/project4/lush.js
+++ project/project4/lush.js
@@ -22,3 +22,39 @@ var swiper = new Swiper(".mySwiper", {
     slideShadows: false,
   },
 });
+
+// 햄버거메뉴
+var burger = $("header .menu-trigger");
+
+burger.each(function (index) {
+  var $this = $(this);
+
+  $this.on("click", function (e) {
+    e.preventDefault();
+    $(this).toggleClass("active-" + (index + 2));
+    $("header").toggleClass("active");
+  });
+});
+
+// 클릭이벤트
+function handleTextClick(event) {
+  var moveText = event.target; // 클릭된 요소 가져오기
+  var randomColor = getRandomColor(); // 랜덤한 색상 생성
+  moveText.style.color = randomColor; // 글자 색상 변경
+}
+
+// 랜덤한 색상 생성 함수
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+// 모든 .move_text 클릭 이벤트 핸들러
+var moveTextElements = document.getElementsByClassName("move_text");
+for (var i = 0; i < moveTextElements.length; i++) {
+  moveTextElements[i].addEventListener("click", handleTextClick);
+}
